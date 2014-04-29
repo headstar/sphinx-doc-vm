6f3ef8bf0808b43063920e97fd900e5e88362142
Per Johansson <per.johansson@post.com>
2014-04-29 12:49:58 +0100

    added another 'extra' package when installing tex

--- site-cookbooks/latex/attributes/default.rb
+++ site-cookbooks/latex/attributes/default.rb
@@ -1,7 +1,8 @@
-# added some 'unnecessary' extra package dependencies to get around the 10 minute package command timeout limit
+# added some 'unnecessary' extra package dependencies as a quick for the 10 minute package command timeout limit
 default['latex']['packages'] = ["texlive-base", 
                                 "texlive-latex-base", 
                                 "texlive-fonts-recommended",
+                                "texlive-fonts-extra-doc",
                                 "texlive-fonts-extra",
                                 "texlive-latex-recommended-doc",
                                 "texlive-latex-recommended",
